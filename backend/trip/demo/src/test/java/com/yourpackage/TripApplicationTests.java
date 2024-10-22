59acc718e1773bae241a644560cd630f86c5654a
solmyst <anushgupta105@gmail.com>
2024-10-22 21:38:40 +0530

    backend

--- backend/trip/demo/src/test/java/com/yourpackage/TripApplicationTests.java
+++ backend/trip/demo/src/test/java/com/yourpackage/TripApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.yourpackage;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class TripApplicationTests {
 
 	@Test
 	void contextLoads() {
